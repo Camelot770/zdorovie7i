8a1449fd10319bc774942424859cc039b6afb2d8
Naum Kogan German <naumkgr@gmail.com>
2026-03-02 17:00:20 +0300

    Fix: add serviceIds to record creation request (required by 1C API)

--- src/pages/ConfirmPage.tsx
+++ src/pages/ConfirmPage.tsx
@@ -47,6 +47,7 @@ export default function ConfirmPage() {
         doctorId,
         specializationId,
         patientId,
+        serviceIds: "",
       });
       navigate("/success");
     } catch (err) {
